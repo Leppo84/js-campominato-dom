15984af584c9ac1f30d3b579ca1f52b0885086b4
LeppoXena <francesco.lepore@xenagames.com>
2022-05-16 16:58:37 +0200

    All broken

--- script.js
+++ script.js
@@ -10,53 +10,140 @@ const mediumField = document.getElementById("medium");
 
 const hardField = document.getElementById("hard");
 
+let allSlot = [];
+
+let bombSlot = [];
+
 // il listener applica la classe e il numero di celle del campo (opzionale, fa scomparire i bottoni e l'header)
 
 easyField.addEventListener("click", 
     function () {
 
-        clearField();
-        fieldGen(100, " easy_slot");
+        let nSlot = 100
+        field.innerHTML = "";
+        fieldGen(nSlot, " easy_slot");
     }
 )
 
 mediumField.addEventListener("click", 
     function () {
 
-        clearField();
-        fieldGen(81, " medium_slot");
+        let nSlot = 81
+        field.innerHTML = "";
+        fieldGen(nSlot, " medium_slot");
     }
 )
 
 hardField.addEventListener("click", 
     function () {
 
-        clearField();
-        fieldGen(49, " hard_slot");
+        let nSlot = 49
+        field.innerHTML = "";
+        fieldGen(nSlot, " hard_slot");
     }
 )
 
 // la funzione crea le cellette dentro il campo
 
-
 function fieldGen(nSlot, slotClass) {
     for (let i = 0; i < nSlot; i++) {
+        while (allSlot.lenght < nSlot ) {
+            let tagSlot = Math.floor(Math.random) * ( nSlot + 1 )
 
-        const slot = document.createElement("div");
-        slot.className = "base_slot" + slotClass;
+            const slot = document.createElement("div");
+            slot.className = "base_slot" + slotClass;
+    
+            field.append(slot);
 
-        field.append(slot)
+            slot.append(tagSlot);
 
-        slot.addEventListener("click",
-        function() {
-            this.classList.add("checked");
+
+            if (!allSlot.includes(tagSlot)) {
+                if (tagSlot <= 16 ) {
+
+                    allSlot.push(tagSlot);
+
+                    bombSlot.push(tagSlot);
+    
+                    const slot = document.createElement("div");
+                    slot.className = "base_slot bomb" + slotClass;
+
+            
+                    field.append(slot);
+                    slot.append(tagSlot);
+                    
+                }
+                else {
+                    allSlot.push(tagSlot);
+                    const slot = document.createElement("div");
+                    slot.className = "base_slot" + slotClass;
+
+                    field.append(slot);
+                    slot.append(tagSlot);        
+                }
+
+
+            }
+            
+            slot.addEventListener("click",
+            function() {
+                this.classList.add("checked");
+            }
+            )
+            
+            
         }
-        )
+        
+
+
+
+
     }
 
 }
 
-// una funzione evita la sovrascrittura
+// !!aggiunta numeri e bombe!!
+
+// creo i numeri univoci non consequenziali generando anche una lista dei 16 "bomba" 
+
+// function bombGen (nslot) {
+//     const allSlot = [];
+//     const bombSlot [];
+//     while (allSlot.lenght < nslot ) {
+//         let tagSlot = Math.floor(Math.random) * ( nslot + 1 )
+//         if (!allSlot.includes(tagSlot)) && tagSlot <= 16 {
+//             allSlot.push(tagSlot);
+//             bombSlot.push(tagSlot);
+//         }
+//         else if (!allSlot.includes(tagSlot)) && tagSlot > 16 {
+//             allSlot.push(tagSlot);            
+//         }
+        
+        
+//     }
+    
+// }
+
+// function randomSlot(nslot) {
+
+    
+// }
+
+// li stampo per capirci qualcosa
+
+// faccio un while per giocare e stampare il punteggio
+
+// se il punteggio arriva al massimo (3 classi di difficoltà) o si scoppia, interrompo il while.
+
+// Si svelano le bombe e si mostra il punteggio 
+
+
+
+
+
+
+
+// una funzione evita la sovrascrittura (in effetti abb inutile)
 
 function clearField() {
     let oldSlot = field.lastElementChild;
